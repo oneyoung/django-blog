8cd29db9e3e7be4bf2d0bf7991564f0fd596d9f2
Oneyoung@Acer4710 <guowangyang@gmail.com>
2012-12-30 14:22:43 +0800

    + 添加Blog类到models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Blog(models.Model):
+    url = models.URLField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modify = models.DateTimeField(auto_now=True)
+    body_html = models.TextField()
+    body_md = models.TextField()
+
+    def __unicode__(self):
+        return self.title
