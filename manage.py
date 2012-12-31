d787fe0d370a0fab0fa7438f2cd6a2900ec21e89
Wangyang Guo <Wangyang_Guo@htc.com>
2012-12-31 17:27:57 +0800

    * 自动将project目录添加到sys.path

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    if project_dir not in sys.path:
+        sys.path.append(project_dir)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogsite.settings")
 
     from django.core.management import execute_from_command_line
