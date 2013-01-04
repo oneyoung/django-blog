17463fb9e45cbeb9a0a5be2b8872268b4b528916
Wangyang Guo <Wangyang_Guo@htc.com>
2013-01-04 17:48:13 +0800

    + 添加blog_view显示特定的文章

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django import http
 from django import forms
 from django.contrib.auth import authenticate, login
+from django.utils import encoding
+import urllib
+from models import Blog
 
 
 class AdminUserForm(forms.Form):
@@ -21,7 +24,20 @@ def admin_login(request):
             if user is not None:
                 if user.is_superuser and user.is_active:
                     login(request, user)
-                    return HttpResponseRedirect('/admin/')
+                    return http.HttpResponseRedirect('/admin/')
         msg = "Invalid username or password, please try again."
     f = AdminUserForm()
     return render(request, 'login.html', {'form': f, 'msg': msg})
+
+
+def blog_view(request, **kwargs):
+    year = int(kwargs.get('year', 0))
+    month = int(kwargs.get('month', 0))
+    title = encoding.smart_unicode(urllib.unquote(kwargs.get('title', '')))
+
+    try:
+        blog = Blog.objects.get(title=title, date_create__year=year,
+                                date_create__month=month)
+        return http.HttpResponse("%s" % blog.body_html)
+    except:
+        raise http.Http404
