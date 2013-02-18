52abd10101ea75c7f675c3e4301365c1cee9eefd
Wayne <guowangyang@gmail.com>
2013-02-18 11:17:53 +0800

    + 去除CGI传进来的script前缀
    Symptom: lighttpd 调用fastcgi时, 会将script name也一起传进来,
    比如 script name: /django-fastcgi,  path: /home/
    url.rewrite-once = (
        "^(/static/.*)$" => "$1",
        "^/favicon\.ico$" => "/static/favicon.ico",
        "^(/.*)$" => "/django_fastcgi$1",
    )
    传递给Django时候, 会被解析成: /django-fastcgi/home/
    Root Cause:
    Solution:
    Project:
    Note:

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -22,6 +22,9 @@ DATABASES = {
     }
 }
 
+# strip script name passed by cgi.
+FORCE_SCRIPT_NAME = ''
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
