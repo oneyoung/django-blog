d615a353e602277656d18adad1c8ab156c4f4b38
Oneyoung@Acer4710 <guowangyang@gmail.com>
2013-04-06 14:59:11 +0800

    + urls: service media files by Django itself in debug mode

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -31,3 +31,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+from blogsite import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, }),
+    )
