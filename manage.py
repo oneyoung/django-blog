141dcc56139c79b513022cd2667a0e8db034d9c7
Wayne <guowangyang@gmail.com>
2013-11-06 15:10:40 +0800

    + manage.py: autoload virutalenv

--- manage.py
+++ manage.py
@@ -2,8 +2,15 @@
 import os
 import sys
 
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+# load virtualenv if necessary
+virtualenv = os.path.join(project_dir, 'virtualenv')
+activate_this = os.path.join(virtualenv, 'bin', 'activate_this.py')
+if os.path.exists(activate_this):
+    execfile(activate_this, dict(__file__=activate_this))
+
 if __name__ == "__main__":
-    project_dir = os.path.dirname(os.path.abspath(__file__))
     if project_dir not in sys.path:
         sys.path.append(project_dir)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogsite.settings")
