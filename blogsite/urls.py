b36cafaa0644ea77a6303311fb01c7d91b141c47
Wayne <guowangyang@gmail.com>
2013-11-12 18:53:45 +0800

    + django admin site

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from blog import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.BlogListView.as_view(), name='home'),
@@ -29,7 +29,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/django/', include(admin.site.urls), name='admin_django'),
 )
 
 from blogsite import settings
